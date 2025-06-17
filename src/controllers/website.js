6f1991913648483d5c1deeeb101e025b6bbc3377
BigB132 <157886921+BigB132@users.noreply.github.com>
2025-06-17 19:26:30 +0000

    Google Shit IDK

--- src/controllers/website.js
+++ src/controllers/website.js
@@ -299,6 +299,7 @@ const landing = async (req, res) => {
         <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="google-site-verification" content="MOa_upgamZEsDEVLWmlDnDJybC7H1uEyHhDrgCECwWI" />
         <title>Welcome to Blearn</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
